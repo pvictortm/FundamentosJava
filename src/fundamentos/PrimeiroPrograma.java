35b6ed1f80cee6c3bd63009a7778fbf41369591b
pvictortm <paulovictor11pv@hotmail.com>
2022-11-15 11:04:20 -0300

    atualizando 1

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -2,7 +2,25 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 
+//COMENTARIO JAVA DOC
+
+ /**
+ * 
+ * @Paulo Victor Torres Martins	<paulovictor11pv@hotmail.com>
+ * @since 1.0
+ */
+	
+	//COMENTANDO UMA SENTENÇA DE CODIGO TERMINADA COM ;
+	//MAIS UM COMENTARIO
+	//FIM
+	
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa!");
+		
+		/*
+		 * LINHA 1
+		 * LINHA 2
+		 * LINHA 3...
+		 */
 	}
 }
